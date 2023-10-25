0e79fedb5f0f8873c2bd684c530b93f290c90e70
mikkelx <michalss1115@gmail.com>
2023-10-25 19:41:39 +0200

    Movie Controller

--- src/main/java/org/example/ApplicationMain.java
+++ src/main/java/org/example/ApplicationMain.java
@@ -1,6 +1,7 @@
 package org.example;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import org.example.controller.MovieController;
 import org.example.dao.GenreDao;
 import org.example.dao.JDBCConnection;
 import org.example.dao.MovieDao;
@@ -8,12 +9,17 @@ import org.example.dao.UserDao;
 import org.example.entity.Genre;
 import org.example.entity.Movie;
 import org.example.entity.User;
+import spark.Spark;
 
 import java.sql.SQLException;
 import java.util.Date;
 
+import static spark.Spark.get;
+
 public class ApplicationMain {
     public static void main(String[] args) {
+
+        /*
         try {
             MovieDao movieDao = MovieDao.getInstance();
             UserDao userDao = UserDao.getInstance();
@@ -31,13 +37,16 @@ public class ApplicationMain {
             }
             User user = new User("test1", "test2", "X", null);
             userDao.getUserDao().create(user);
-//            for (User user1 : userDao.getUserDao().queryForAll()) {
-//                System.out.println(user1);
-//            }
+            for (User user1 : userDao.getUserDao().queryForAll()) {
+                System.out.println(user1);
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-//        dao.getAll();
-//        get("/hello", (req, res) -> "Hello World");
+        */
+        Spark.init();
+        MovieController.registerRoutes();
+        Spark.awaitInitialization();
+        get("/hello", (req, res) -> "Hello World");
     }
 }
---
+++ src/main/java/org/example/controller/MovieController.java
@@ -0,0 +1,84 @@
+package org.example.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import org.example.dao.MovieDao;
+import org.example.entity.Movie;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static spark.Spark.*;
+
+public class MovieController {
+
+    private static MovieDao movieDao = MovieDao.getInstance();
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        // data w formacie "yyyy-MM-dd"
+        objectMapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
+    public static Route getAllMovies = (Request request, Response response) -> {
+        response.type("application/json");
+        List<Movie> movies = movieDao.getMovieDao().queryForAll();
+        return objectMapper.writeValueAsString(movies);
+    };
+
+    public static Route getMovieById = (Request request, Response response) -> {
+        response.type("application/json");
+        int movieId = Integer.parseInt(request.params("movieId"));
+        Movie movie = movieDao.getMovieDao().queryForId(movieId);
+        if (movie != null) {
+            return objectMapper.writeValueAsString(movie);
+        } else {
+            response.status(404);
+            return "Film o podanym ID nie został znaleziony.";
+        }
+    };
+
+    public static Route createMovie = (Request request, Response response) -> {
+        response.type("application/json");
+        Movie newMovie = objectMapper.readValue(request.body(), Movie.class);
+        try {
+            movieDao.getMovieDao().create(newMovie);
+            response.status(201); // Status 201 Created
+            return objectMapper.writeValueAsString(newMovie);
+        } catch (SQLException e) {
+            response.status(500);
+            return "Wystąpił błąd podczas zapisywania filmu.";
+        }
+    };
+
+    public static Route deleteMovie = (Request request, Response response) -> {
+        int movieId = Integer.parseInt(request.params("movieId"));
+        try {
+            int deleted = movieDao.getMovieDao().deleteById(movieId);
+            if (deleted == 1) {
+                response.status(200);
+                return "";
+            } else {
+                response.status(404);
+                return "Film o podanym ID nie został znaleziony.";
+            }
+        } catch (SQLException e) {
+            response.status(500);
+            return "Wystąpił błąd podczas usuwania filmu.";
+        }
+    };
+
+    public static void registerRoutes() {
+        get("/movies", getAllMovies);
+        get("/movies/:movieId", getMovieById);
+        post("/movies", createMovie);
+        delete("/movies/:movieId", deleteMovie);
+    }
+
+}
